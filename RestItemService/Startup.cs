b463071bd564dadf5eda20a684d09ef13cf178c9
Arsen Niyonzima <arse0012@edu.easj.dk>
2020-10-05 14:30:46 +0200

    UnitTest, Swagger and launchsetting

--- RestItemService/Startup.cs
+++ RestItemService/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace RestItemService
 {
@@ -25,6 +26,42 @@ namespace RestItemService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder.WithOrigins("http://example.com"));
+
+                options.AddPolicy("AllowAnyOrigin",
+                    builder => builder.AllowAnyOrigin());
+
+                options.AddPolicy("AllowAnyOriginGetPost",
+                    builder => builder.AllowAnyOrigin().WithMethods("GET", "POST"));
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Items API",
+                    Version = "v1.0",
+                    Description = "Example of OpenAPI for api/localItems",
+                    TermsOfService = new Uri("/api/Items"),
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "{Arsen}",
+                        Email = "",
+                        Url = new Uri("http://localhost:5000") 
+                    },
+                    License = new OpenApiLicense()
+                    {
+                        Name="No licence required",
+                        Url = new Uri("/api/Items")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +71,14 @@ namespace RestItemService
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Items API v1.0");
+                    c.RoutePrefix = "api/help";
+                }
+            );
 
             app.UseRouting();
 
---
+++ RestItemUnitTest/ItemsTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLib.Model;
+
+namespace RestItemUnitTest
+{
+    [TestClass]
+    public class ItemsTest
+    {
+        [TestMethod]
+        public void GetTest()
+        {
+            Item item = new Item();
+            item.Id = 7;
+            Assert.AreEqual(7, item.Id);
+        }
+
+        [TestMethod]
+        public void GetFromSubstringTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetQualityTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetFilterTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PostTest()
+        {
+
+        }
+        [TestMethod]
+        public void PutTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+
+        }
+    }
+}
